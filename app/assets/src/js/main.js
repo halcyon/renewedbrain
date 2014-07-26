f4a2c46d34142a8faa0002376006084b07a6d064
Scott McLeod <halcyonblue@gmail.com>
2014-07-26 17:34:35 -0400

    Remove need for analytics

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
   console.log('about to load!');
-  $('#analytics').getScript('app/assets/src/js/google-analytics.js');
+  $.getScript('app/assets/src/js/google-analytics.js');
 });
