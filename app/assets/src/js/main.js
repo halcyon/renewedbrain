e89d2036dc08199e1c7c18ebdc6cee0b63591100
Scott McLeod <halcyonblue@gmail.com>
2014-07-26 17:28:57 -0400

    Replace load with getScript

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,2 +1,4 @@
-console.log('about to load!');
-$('#analytics').load('app/assets/src/js/google-analytics.js');
+$(document).ready(function(){
+  console.log('about to load!');
+  $('#analytics').getScript('app/assets/src/js/google-analytics.js');
+});
