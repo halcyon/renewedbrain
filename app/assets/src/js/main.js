66790daa8e535787694bb6f16dc9724c1827a3cf
Scott McLeod <halcyonblue@gmail.com>
2014-07-26 17:22:41 -0400

    Remove document.ready

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,4 +1,2 @@
-$(document).ready(function(){
-  console.log('about to load!');
-  $('#analytics').load('app/assets/src/js/google-analytics.js');
-});
+console.log('about to load!');
+$('#analytics').load('app/assets/src/js/google-analytics.js');
